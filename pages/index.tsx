540e61b94b4ec15abb79cd2b245ff87b840570ae
Vignesh Ashokan <vigneshashokan@gmail.com>
2023-10-09 20:38:20 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Vignesh's Portfolio</title>
+      </Head>
+      <h1 className="text-red-500">Let's build a portfolio</h1>
+    </div>
+  );
+}
