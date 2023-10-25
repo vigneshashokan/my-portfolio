57832dbd5b48ec78d2cd56d9c2277d703d857089
vigneshashokan <vignesh.ashokan@protonmail.com>
2023-10-25 09:11:20 -0700

    image to icons for skills (#9)
    Co-authored-by: Vignesh Ashokan <vigneshashokan@gmail.com>

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,27 +1,28 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { Logo } from "@/typings";
+import { Icon } from "@/typings";
 
 type Props = {
   directionLeft?: boolean;
-  skill: Logo;
+  skill: Icon;
 };
 
 export default function Skill({ directionLeft, skill }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
+      <motion.div
         initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 1 }}
-        src={skill.src}
-        className="object-cover h-24 w-24 md:h-28 md:w-28 filter group-hover:grayscale transition duration-300 ease-in-out"
-      />
-      <div className="absolute opacity-0 group-hover:opacity-60 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0">
+        className="object-cover h-24 w-24 filter group-hover:grayscale ease-in-out text-7xl group-hover:text-gray-600 flex items-center justify-center"
+      >
+        <div>{skill[1]}</div>
+      </motion.div>
+      {/* <div className="absolute opacity-0 group-hover:opacity-40 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl font-bold text-black opacity-100">100%</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- typings.d.ts
+++ typings.d.ts
@@ -3,10 +3,13 @@ export type Logo = {
   alt?: string;
 };
 
+export type Icon = [string, JSX.Element];
+
 export type Experience = {
   companyName: string;
   companyLogo: Logo;
   position: string;
+  techStack: [string];
   techLogos: [Logo];
   period: string;
   jobDuties: [string];
