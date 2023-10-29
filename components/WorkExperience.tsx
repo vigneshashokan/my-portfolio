0c8c9594e1a0fd3d3324ca40cdfe018dc94806d8
vigneshashokan <vignesh.ashokan@protonmail.com>
2023-10-29 16:13:03 -0700

    final commit (#13)
    Co-authored-by: Vignesh Ashokan <vigneshashokan@gmail.com>

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,6 +1,10 @@
 import { motion } from "framer-motion";
 import WorkExperienceCard from "./WorkExperienceCard";
 import { Experience } from "@/typings";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
 
 type Props = {
   experiences: [Experience];
@@ -15,12 +19,18 @@ const WorkExperience = ({ experiences }: Props) => {
       className="flex flex-col md:flex-row relative h-screen max-w-full px-5 md:px-10 mx-auto justify-evenly items-center
       overflow-hidden text-left"
     >
-      <h3 className="absolute top-16 md:top-24 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-2xl">
-        Experience
-      </h3>
+      <div className="flex absolute top-16 md:top-24 uppercase tracking-[5px] md:tracking-[20px] text-gray-500 text-2xl">
+        <div className="md:hidden p-1 mr-5">
+          <BsFillArrowLeftCircleFill />
+        </div>
+        <h3>Experience</h3>
+        <div className="md:hidden p-1 ml-3">
+          <BsFillArrowRightCircleFill />
+        </div>
+      </div>
       <div
-        className="w-screen mt-24 md:mt-0 md:w-full flex space-x-5 md:p-10 overflow-x-scroll snap-x snap-mandatory scrollbar
-        scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+        className="w-screen mt-24 md:mt-0 md:w-full flex space-x-5 md:p-10 overflow-x-scroll snap-x snap-mandatory
+        scrollbar scrollbar-thumb-[#F7AB0A]/80"
       >
         {experiences.map((exp, i) => (
           <WorkExperienceCard key={i} experience={exp} />
